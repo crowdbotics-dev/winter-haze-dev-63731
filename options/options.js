8d8976ad938deb8c3d3c8d40f0c35d14edf27445
Crowdbotics <engineering@crowdbotics.com>
2023-02-15 04:46:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "winter-haze-dev-63731",
+  url: "https://winter-haze-dev-63731.botics.co",
+  api: "https://winter-haze-dev-63731.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
